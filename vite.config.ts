43e99a592bc9cec3854edee4913128b5dbcb3f69
Cobrazking <120334498+Cobrazking@users.noreply.github.com>
2025-06-12 22:22:32 +0200

    Oppdate
    tetete

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname, 'kalk'),
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  build: {
-    outDir: path.resolve(__dirname, 'kalk/dist'),
-    emptyOutDir: true,
   },
 });
